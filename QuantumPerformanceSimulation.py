b4ec39ff0725728766ab8bc0b22caa1a9b03cd29
eggjpeg <kolya46592@gmail.com>
2024-05-09 20:24:33 -0400

    Implemented simulation with Gaussian Conditional Independence Model

--- QuantumPerformanceSimulation.py
+++ QuantumPerformanceSimulation.py
@@ -3,7 +3,7 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 from qiskit_finance.data_providers import BaseDataProvider
-
+from qiskit_finance.circuit.library import GaussianConditionalIndependenceModel
 class StockDataProcessor(BaseDataProvider):
     def __init__(self, file_path, tickers, start, end):
         """
@@ -105,6 +105,28 @@ plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.1), ncol=3)
 plt.xticks(rotation=90)
 plt.savefig('foo2.png')
 
-# Computing covariance matrix
-sigma = data.get_period_return_covariance_matrix()
-print(sigma)
+
+# Example placeholders for functionality not implemented here
+mean_vector = data.get_period_return_mean_vector() 
+cov_matrix = data.get_period_return_covariance_matrix() 
+std_devs = np.sqrt(np.diag(cov_matrix))
+
+# Calculate the correlation matrix
+correlation_matrix = cov_matrix / np.outer(std_devs, std_devs)
+flattened = list(set(correlation_matrix.flatten()))
+flattened.remove(1.0)
+annual_expected_returns = np.array([0.1, 0.1, 0.06])
+monthly_expected_log_returns = np.log(1 + annual_expected_returns) / 12
+
+# Gaussian Conditional Independence Model parameters
+n_normal = 3  # Number of qubits to represent the latent normal random variable Z
+normal_max_value = 3  # Truncate the latent normal random variable Z between +/- this value
+
+# Probabilities and sensitivities to latent variable
+p_zeros = np.array([0.1, 0.1, 0.06])  # Standard default probabilities
+rhos = np.array([0.3, 0.3, 0.2])  # Sensitivities of default probabilities
+# Create Gaussian conditional independence model
+print(monthly_expected_log_returns)
+print(correlation_matrix[0])
+model = GaussianConditionalIndependenceModel(n_normal, normal_max_value, monthly_expected_log_returns, flattened)
+
